6e1c421d71a0ed4ab208d038a523cd7e767eb5b0
MOHAMED ZIENHOM <49864773+zienhom1994@users.noreply.github.com>
2020-11-18 04:06:03 +0200

    Update 06_homework_04_answer.cpp

--- 18-Programming-4kids/06_homework_04_answer.cpp
+++ 18-Programming-4kids/06_homework_04_answer.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
+int n;
+cin >> n;
+int d4 = n % 10000;
+int befor = n % 1000;
+int d4sub = d4 - befor;
+int d4div = d4sub / 1000;
+cout << "digit n4 is : " << d4 << endl;
+cout << "digit befor n4 is :  " << befor << endl;
+cout << "digit sub n4 is :  " << d4sub << endl;
+cout << "digit n4 only is :  " << d4div <<  endl;
 
-	int n;
-	cin >> n;
-
-	// /1000 => removes last 3 digits
-	// %10 get next digit = 4th
-	cout << (n / 1000) % 10 << "\n";
 
 	return 0;
 }
